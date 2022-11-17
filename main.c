17b9ad1fafdd0992e03ccc6a685676bd7781a05e
Giuseppe <fanuli.giuseppe99@gmail.com>
2022-11-17 17:16:23 +0100

    Genarted all the possible combinations

--- main.c
+++ main.c
@@ -1,258 +1,312 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define NUM_GIORNATE 18
 
-typedef struct{
-    int id;
-    char nome[50];
-    float punteggio[19];
-    float sum;
-}Squadra;
-
-typedef struct{
-    Squadra A;
-    Squadra B;
-}Partita;
-
-typedef struct{
-    Partita *p;
-}Giornata;
-
-void shiftLeft(Squadra *data, Squadra add,int len);
-Squadra shiftRight(Squadra *data, Squadra add,int len);
-int perm_sempl(int pos,Giornata *val,Giornata *sol,int *mark,int n,int n_squadre, int count);
-int perm_sempl_squad(int pos,Squadra *val,Squadra *sol,int *mark,int n,int n_squadre, int count);
-Squadra *leggiFile(int n);
-void calcola_classifica(Giornata *sol,int n,int n_teams);
-int goal(float punteggio);
-
-int *n_wins;
-int *n_silver;
-int *n_bronze;
-Squadra *teams;
-
-int main(int argc,char** argv)
+typedef struct Team
 {
-    char space[30]=" ";
-    int numero_squadre=atoi(argv[1]);
-    int giornate=numero_squadre-1;
-    Giornata *g;
-    Giornata *sol;
-    Squadra *sol_squad;
-    int *mark;
-    int count=0;
-    Squadra *casa;
-    Squadra *trasferta;
-    Squadra *teams_temp;
+    char *name;
+} Team;
 
-    g=malloc(giornate*sizeof(Giornata));
-    sol=malloc(giornate*sizeof(Giornata));
-    for(int i=0;i<giornate;i++){
-        g[i].p=malloc((numero_squadre/2)*sizeof(Partita));
-        sol[i].p=malloc((numero_squadre/2)*sizeof(Partita));
-    }
-    mark=calloc(giornate,sizeof(int));
-    n_wins=calloc(numero_squadre,sizeof(int));
-    n_silver=calloc(numero_squadre,sizeof(int));
-    n_bronze=calloc(numero_squadre,sizeof(int));
-    casa=malloc(numero_squadre*sizeof(Squadra));
-    trasferta=malloc(numero_squadre*sizeof(Squadra));
-    teams_temp=malloc(numero_squadre*sizeof(Squadra));
-    sol_squad=malloc(numero_squadre*sizeof(Squadra));
+typedef struct
+{
+    Team home;
+    Team away;
+} Match;
 
-    teams=leggiFile(atoi(argv[1]));
-    for(int i=0;i<numero_squadre;i++)
-        teams_temp[i]=teams[i];
+typedef struct
+{
+    Match *matches;
+} Matchweek;
 
-    for(int k=0;k<numero_squadre/2;k++){
-        for (int i = 0; i < numero_squadre /2; i++) {
-            casa [i] = teams_temp[i];
-            trasferta[i] = teams_temp[numero_squadre - 1 - i];
-        }
+typedef struct
+{
+    Matchweek matchweek[3];
+} Round;
 
-        for (int i = 0; i < giornate; i++) {
-            /* Stampa le partite di questa giornata */
-            //printf("%d^ Giornata\n",i+1);
+void generateSchedules(Team *teams, int n_teams);
+int comb_sempl_match(int pos, Team *val, Team *sol, int n, int k, int start, int count, Match *saved);
+int comb_sempl_week(int pos, Match *val, Match *sol, int n, int k, int start, int count, Matchweek *saved);
+int perm_sempl_round(int pos, Matchweek *val, Matchweek *sol, int *mark, int n, int count);
+void printMatch(Match *matches, int size);
+void printMatchWeek(Matchweek *mw, int size, int n_matches);
+int validate(Match *sol, int n_matches);
 
-            /* Alterna le partite in casa e fuori */
-            if ((i % 2) == 0) {
-                for (int j = 0; j < numero_squadre /2 ; j++){
-                    g[i].p[j].A=trasferta[j];
-                    g[i].p[j].B=casa[j];
-                    //printf("%d.  %d - %d\n", j+1, trasferta[j], casa[j]);
-                }
-            }
-            else {
-                for (int j = 0; j < numero_squadre /2 ; j++){
-                    g[i].p[j].A=casa[j];
-                    g[i].p[j].B=trasferta[j];
-                    //printf("%d.  %d - %d\n", j+1, casa[j], trasferta[j]);
-                }
-            }
+int main()
+{
+    Team teams[10];
 
-            // Ruota in gli elementi delle liste, tenendo fisso il primo elemento
-            // Salva l'elemento fisso
-            Squadra pivot = casa [0];
+    teams[0] = (Team){"A"};
+    teams[1] = (Team){"B"};
+    teams[2] = (Team){"C"};
+    teams[3] = (Team){"D"};
+    teams[4] = (Team){"E"};
+    teams[5] = (Team){"F"};
+    teams[6] = (Team){"G"};
+    teams[7] = (Team){"H"};
+    teams[8] = (Team){"I"};
+    teams[9] = (Team){"J"};
 
-            /* Sposta in avanti gli elementi di "trasferta" inserendo
-            all'inizio l'elemento casa[1] e salva l'elemento uscente in "riporto" */
-            Squadra riporto = shiftRight(trasferta, casa [1],numero_squadre/2);
+    int n_teams = (sizeof(teams) / sizeof(Team));
+    generateSchedules(teams, n_teams);
 
-            /* Sposta a sinistra gli elementi di "casa" inserendo all'ultimo
-            posto l'elemento "riporto" */
-            shiftLeft(casa, riporto,numero_squadre/2);
-
-            // Ripristina l'elemento fisso
-            casa[0] = pivot ;
-        }
-
-        //Chiamo permutazioni semplici sulle giornate
-        count+=perm_sempl(0,g,sol,mark,giornate,numero_squadre,0);
-        //Scambio una partita in casa con una in trasferta
-        shiftLeft(teams_temp,teams_temp[0],numero_squadre);
-        //printf("\n");
-    }
-    printf("Calendari trovati = %d\n\n",count);
-    printf("NOME SQUADRA\t\t\tPRIMO\t\tSECONDO\t\tTERZO\t\tVOLTE A PREMIO\n\n");
-    for(int i=0;i<numero_squadre;i++){
-        for(int j=0;j<30-strlen(teams[i].nome);j++)
-            strcat(space," ");
-        printf("%s%s %.2f%% (%d)\t%.2f%% (%d)\t%.2f%% (%d)\t%.2f%% (%d)\n\n",
-               teams[i].nome,space,(float)n_wins[i]/count*100,n_wins[i],(float)n_silver[i]/count*100,n_silver[i],
-               (float)n_bronze[i]/count*100,n_bronze[i],
-               (float)(n_wins[i]+n_silver[i]+n_bronze[i])/count*100,(n_wins[i]+n_silver[i]+n_bronze[i]));
-        strcpy(space,"");
-    }
     return 0;
 }
 
-void shiftLeft(Squadra *data, Squadra add,int len){
-    Squadra temp[len];
-    for (int i = 0; i < len; i++) {
-			temp[i] = data[i + 1];
+void generateSchedules(Team *teams, int n_teams)
+{
+    Team teams_sol[2];    // Internal variable used for computing team combinations
+    Match *matchweek_sol; // Internal variable for computing match combination
+    Matchweek *round_sol;
+    int *mark;
+
+    Match *possible_team_pairs;
+    Matchweek *possible_matchweeks;
+    Matchweek *matchweek_set;
+
+    int count;
+    int n_matchweeks;
+    int found = 0;
+    int size = 0;
+
+    switch (n_teams)
+    {
+    case 4:
+        n_matchweeks = 3;
+        possible_team_pairs = malloc(6 * sizeof(Match));                // Mathematically computed
+        possible_matchweeks = malloc(n_matchweeks * sizeof(Matchweek)); // Mathematically computed
+        matchweek_set = malloc(n_matchweeks * sizeof(Matchweek));       // Mathematically computed
+        round_sol = malloc(n_matchweeks * sizeof(Matchweek));
+        mark = malloc(n_matchweeks * sizeof(int));
+        for (int i = 0; i < 3; i++)
+        {
+            possible_matchweeks[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            matchweek_set[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            round_sol[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            mark[i] = 0;
+        }
+        break;
+    case 6:
+        n_matchweeks = 15;
+        possible_team_pairs = malloc(15 * sizeof(Match));               // Mathematically computed
+        possible_matchweeks = malloc(n_matchweeks * sizeof(Matchweek)); // Mathematically computed
+        matchweek_set = malloc(n_matchweeks * sizeof(Matchweek));       // Mathematically computed
+        round_sol = malloc(n_matchweeks * sizeof(Matchweek));
+        mark = malloc(n_matchweeks * sizeof(int));
+        for (int i = 0; i < n_matchweeks; i++)
+        {
+            possible_matchweeks[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            matchweek_set[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            round_sol[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            mark[i] = 0;
+        }
+
+        break;
+    case 8:
+        n_matchweeks = 105;
+        possible_team_pairs = malloc(28 * sizeof(Match));               // Mathematically computed
+        possible_matchweeks = malloc(n_matchweeks * sizeof(Matchweek)); // Mathematically computed
+        matchweek_set = malloc(n_matchweeks * sizeof(Matchweek));       // Mathematically computed
+        round_sol = malloc(n_matchweeks * sizeof(Matchweek));
+        mark = malloc(n_matchweeks * sizeof(int));
+        for (int i = 0; i < n_matchweeks; i++)
+        {
+            possible_matchweeks[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            matchweek_set[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            round_sol[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            mark[i] = 0;
+        }
+        break;
+    case 10:
+        n_matchweeks = 945;
+        possible_team_pairs = malloc(45 * sizeof(Match));               // Mathematically computed
+        possible_matchweeks = malloc(n_matchweeks * sizeof(Matchweek)); // Mathematically computed
+        matchweek_set = malloc(n_matchweeks * sizeof(Matchweek));       // Mathematically computed
+        round_sol = malloc(n_matchweeks * sizeof(Matchweek));
+        mark = malloc(n_matchweeks * sizeof(int));
+        for (int i = 0; i < n_matchweeks; i++)
+        {
+            possible_matchweeks[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            matchweek_set[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            round_sol[i].matches = malloc(n_teams / 2 * sizeof(Match));
+            mark[i] = 0;
+        }
+        break;
+    default:
+        return;
+    };
+    matchweek_sol = malloc((n_teams / 2) * sizeof(Match));
+
+    count = comb_sempl_match(0, teams, teams_sol, n_teams, 2, 0, 0, possible_team_pairs);
+    // printf("Combinazioni: %d\n", count);
+    //  printMatch(possible_team_pairs, count);
+
+    count = comb_sempl_week(0, possible_team_pairs, matchweek_sol, count, n_teams / 2, 0, 0, possible_matchweeks);
+    printf("Combinazioni: %d\n", count);
+    // printMatchWeek(possible_matchweeks, count, n_teams / 2);
+
+    count = 0;
+    for (int l = 0; l < n_matchweeks; l++)
+    {
+        for (int i = 0; i < n_matchweeks; i++)
+        {
+            matchweek_set[i].matches = (void *)0;
+            // free(matchweek_set[i].matches);
+        }
+        for (int i = 0; i < n_matchweeks; i++)
+        {
+            matchweek_set[i].matches = malloc(n_teams / 2 * sizeof(Match));
+        }
+        size = 0;
+        found = 0;
+        matchweek_set[size++].matches = possible_matchweeks[l].matches;
+
+        for (int i = 0; i < n_matchweeks; i++)
+        {
+            // Scan all the combinations
+            for (int j = 0; j < size; j++)
+            {
+                // Scan the current set of combination
+                for (int h = 0; h < n_teams / 2 && !found; h++)
+                {
+                    // Scan matches of a given matchweek
+                    for (int k = 0; k < n_teams / 2 && !found; k++)
+                    {
+                        // Scan matches of a given matchweek inside the set
+                        if ((strcmp(possible_matchweeks[i].matches[h].home.name, matchweek_set[j].matches[k].home.name) == 0 &&
+                             strcmp(possible_matchweeks[i].matches[h].away.name, matchweek_set[j].matches[k].away.name) == 0) ||
+                            (strcmp(possible_matchweeks[i].matches[h].home.name, matchweek_set[j].matches[k].away.name) == 0 &&
+                             strcmp(possible_matchweeks[i].matches[h].away.name, matchweek_set[j].matches[k].home.name) == 0))
+                        {
+                            // Matchweek already in the set
+                            found = 1;
+                        }
+                    }
+                }
+            }
+            if (!found)
+            {
+                matchweek_set[size++].matches = possible_matchweeks[i].matches;
+            }
+            found = 0;
+        }
+        count = perm_sempl_round(0, matchweek_set, round_sol, mark, n_teams - 1, count);
+        // printMatchWeek(matchweek_set, size, n_teams / 2);
+        // count = count + 1;
     }
-    temp[len-1] = add;
-    for(int i=0;i<len;i++){
-        data[i]=temp[i];
-    }
-    return;
+    // printf("Combinazioni: %d\n", count);
+    printf("Permutazioni: %d\n", count);
 }
 
-Squadra shiftRight(Squadra *data, Squadra add,int len){
-    Squadra temp[len];
-    Squadra riporto = data[1];
-    for (int i = 1; i < len; i++) {
-        temp[i] = data[i - 1];
-    }
-    temp[0] = add;
-
-    for(int i=0;i<len+1;i++)
-        data[i]=temp[i];
-
-    return riporto;
-}
-
-int perm_sempl(int pos,Giornata *val,Giornata *sol,int *mark,int n,int n_squadre, int count){
+int comb_sempl_match(int pos, Team *val, Team *sol, int n, int k, int start, int count, Match *saved)
+{
     int i;
 
-    if(pos>=n){
-        //Calcolare la classifica finale
-        calcola_classifica(sol,n,n_squadre);
-        return count+1;
+    if (pos >= k)
+    {
+        saved[count].home = sol[0];
+        saved[count].away = sol[1];
+        return count + 1;
     }
+    for (i = start; i < n; i++)
+    {
+        sol[pos] = val[i];
+        count = comb_sempl_match(pos + 1, val, sol, n, k, i + 1, count, saved);
+    }
+    return count;
+}
 
-    for(i=0;i<n;i++){
-        if(mark[i]==0){
-            mark[i]=1;
-            sol[pos]=val[i];
-            count=perm_sempl(pos+1,val,sol,mark,n,n_squadre,count);
-            mark[i]=0;
+int comb_sempl_week(int pos, Match *val, Match *sol, int n, int k, int start, int count, Matchweek *saved)
+{
+    int i;
+
+    if (pos >= k)
+    {
+        if (validate(sol, k))
+        {
+            return count;
+        }
+
+        for (i = 0; i < k; i++)
+        {
+            saved[count].matches[i].home = sol[i].home;
+            saved[count].matches[i].away = sol[i].away;
+            printf("%s - %s", saved[count].matches[i].home.name, saved[count].matches[i].away.name);
+            i != k - 1 ? printf(" --- ") : 0;
+        }
+        printf("\n");
+
+        return count + 1;
+    }
+    for (i = start; i < n; i++)
+    {
+        sol[pos].home = val[i].home;
+        sol[pos].away = val[i].away;
+        count = comb_sempl_week(pos + 1, val, sol, n, k, i + 1, count, saved);
+    }
+    return count;
+}
+
+int perm_sempl_round(int pos, Matchweek *val, Matchweek *sol, int *mark, int n, int count)
+{
+    int i;
+    if (pos >= n)
+    {
+        // TODO
+        printf("%d\r", count + 1);
+        return count + 1;
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (mark[i] == 0)
+        {
+            mark[i] = 1;
+            sol[pos] = val[i];
+            count = perm_sempl_round(pos + 1, val, sol, mark, n, count);
+            mark[i] = 0;
         }
     }
     return count;
 }
-Squadra *leggiFile(int n){
-    Squadra *t;
-    int count=0;
-    FILE *fin;
 
-    //Se il programma è lanciato da linea di comando
-    //fin = fopen("../../Mollo_Cup.txt","r");
-
-    //Se il programma è lanciato da IDE
-    fin = fopen("Mollo_Cup_ritorno.txt","r");
-
-    t=malloc(n*sizeof(Squadra));
-
-    while(count<n){
-        fscanf(fin,"%s",t[count].nome);
-        t[count].id=count;
-        t[count].sum=0;
-        for(int i=0;i<NUM_GIORNATE;i++){
-            fscanf(fin,"%f",&(t[count].punteggio[i]));
-            t[count].sum+=t[count].punteggio[i];
-        }
-        count++;
-    }
-    fclose(fin);
-    return t;
+void printMatch(Match *matches, int size)
+{
+    // for (int i = 0; i < size; i++)
+    // {
+    //     printf("%s - %s\n", matches[i].home, matches[i].away);
+    // }
+    printf("Combinazioni: %d\n", size);
 }
 
-void calcola_classifica(Giornata *sol,int n,int n_teams){
-    int *classifica;
-    int first=-1,second=-1,third=-1,id1,id2,id3;
+void printMatchWeek(Matchweek *mw, int size, int n_matches)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < n_matches; j++)
+        {
+            printf("%s - %s", mw[i].matches[j].home.name, mw[i].matches[j].away.name);
+            j != n_matches - 1 ? printf(" --- ") : 0;
+            // printf("%s - %s --- ", mw[i].matches[0].home, mw[i].matches[0].away);
+            // printf("%s - %s\n", mw[i].matches[1].home, mw[i].matches[1].away);
+        }
+        printf("\n");
+    }
+    printf("Combinazioni: %d\n", size);
+}
 
-    classifica=calloc(n_teams,sizeof(int));
-    for(int i=0;i<NUM_GIORNATE;i++){
-        for(int j=0;j<n_teams/2;j++){
-            if(goal(sol[i%(n_teams-1)].p[j].A.punteggio[i])>goal(sol[i%(n_teams-1)].p[j].B.punteggio[i]))
-                classifica[sol[i%(n_teams-1)].p[j].A.id]+=3;
-            else if(goal(sol[i%(n_teams-1)].p[j].A.punteggio[i])<goal(sol[i%(n_teams-1)].p[j].B.punteggio[i]))
-                classifica[sol[i%(n_teams-1)].p[j].B.id]+=3;
-            else{
-                //Pareggio
-                classifica[sol[i%(n_teams-1)].p[j].B.id]+=1;
-                classifica[sol[i%(n_teams-1)].p[j].A.id]+=1;
+int validate(Match *sol, int n_matches)
+{
+    for (int i = 0; i < n_matches; i++)
+    {
+        for (int j = i + 1; j < n_matches; j++)
+        {
+            if (strcmp(sol[i].home.name, sol[j].home.name) == 0 ||
+                strcmp(sol[i].home.name, sol[j].away.name) == 0 ||
+                strcmp(sol[i].away.name, sol[j].home.name) == 0 ||
+                strcmp(sol[i].away.name, sol[j].away.name) == 0)
+            {
+                return 1;
             }
         }
     }
-
-    for(int i=0;i<n_teams;i++){
-        if((classifica[i]==first && teams[i].sum>teams[id1].sum)||(classifica[i]>first)){
-            third=second;
-            second=first;
-            first=classifica[i];
-            id3=id2;
-            id2=id1;
-            id1=i;
-        }
-        else if((classifica[i]==second && teams[i].sum>teams[id2].sum)||(classifica[i]>second)){
-            third=second;
-            second=classifica[i];
-            id3=id2;
-            id2=i;
-        }
-        else if((classifica[i]==third && teams[i].sum>teams[id3].sum)||(classifica[i]>third)){
-            third=classifica[i];
-            id3=i;
-        }
-    }
-    n_wins[id1]++;
-    n_silver[id2]++;
-    n_bronze[id3]++;
-    return;
-}
-
-int goal(float punteggio){
-    float base=66.0;
-    int gol=1;
-    if(punteggio<base)
-        return 0;
-
-    while(base+4.0<punteggio){
-        base+=4.0;
-        gol++;
-    }
-    return gol;
-}
+    return 0;
+};
